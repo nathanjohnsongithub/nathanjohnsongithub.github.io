d55f69b2bb042587e287d3370d7dbb17d5552691
Nathan W Johnson <Nathanjohnson482@gmail.com>
2024-11-21 21:25:59 -0600

    maybe working mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.js"],
+  content: [
+    "./index.html",
+    "./script.js", // Ensure this includes the script if you're using dynamic classes
+    // Add other paths as necessary
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-
+}
